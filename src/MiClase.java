3366d927cd1d61ecdad9d6ec0e21c02f6a17d740
orhel <orhel7@gmail.com>
2015-02-16 20:27:45 -0600

    Update MiClase.java

--- src/MiClase.java
+++ src/MiClase.java
@@ -9,12 +9,12 @@ public class MiClase {
 	//devuelve "x" multiplicado por dos
 	int getXPor2()
 	{
-		return (x*2);
+		return (x*2);//"x" multiplicado por 2
 	}
 	
 	//devuelve la suma de "x" y "y"
 	int getXMasY(int y)
 	{
-		return (x+y);
+		return (x+y);//suma de "x" + "y"
 	}
 }
